e22e262337cedad297aa10a2a11a831a97d8fef7
jass5757 <jasminesihu72654@gmail.com>
2025-11-24 16:44:52 +0300

    completed he lab work

--- LAB05/project-spa-backend/routes/projects.js
+++ LAB05/project-spa-backend/routes/projects.js
@@ -15,4 +15,47 @@ router.get('/', async (req, res) => {
     }
 });
 
+// POST - Create new project
+router.post('/', async (req, res) => {
+    try {
+        const newProject = await Project.create(req.body);
+        res.status(201).json(newProject);
+    } catch (error) {
+        res.status(501).json({ 
+            message: 'Error creating project', 
+            error: error.message 
+        });
+    }
+});
+
+// PUT - Update project
+router.put('/', async (req, res) => {
+    try {
+        const updatedProject = await Project.findOneAndUpdate(
+            { _id: req.body._id },
+            req.body,
+            { new: true }
+        );
+        res.status(202).json(updatedProject);
+    } catch (error) {
+        res.status(400).json({ 
+            message: 'Error updating project', 
+            error: error.message 
+        });
+    }
+});
+
+// DELETE - Remove project
+router.delete('/:_id', async (req, res) => {
+    try {
+        await Project.deleteOne({ _id: req.params._id });
+        res.status(204).send();
+    } catch (error) {
+        res.status(400).json({ 
+            message: 'Error deleting project', 
+            error: error.message 
+        });
+    }
+});
+
 module.exports = router;
